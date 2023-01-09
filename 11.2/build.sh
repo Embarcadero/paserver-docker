70a9f6ce1afe5a0f6b726a55c04d1e2177b89862
Serhii Kheilyk <1171495+lynxnake@users.noreply.github.com>
2023-01-09 14:29:49 +0200

    Add alexandria and latest tags
    both might be changed later:
    'alexandria' means latest 11.x version (now it is 11.2)
    'latest' means latest version from all
     (e.g. 12.0 when it will be issued)

--- 11.2/build.sh
+++ 11.2/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build . --build-arg password=securepass --tag=radstudio/paserver:11.2
+docker build . --build-arg password=securepass --tag=radstudio/paserver:11.2 \
+    --tag=radstudio/paserver:alexandria --tag=radstudio/paserver:latest
