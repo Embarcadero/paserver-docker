cdffeb0bfb8afef4582cd2110c85436801af3832
Jim McKeeth <jim@mckeeth.org>
2023-03-29 13:08:00 -0600

    formatting

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build . --build-arg password=securepass --tag=radstudio/paserver:11.3 \
-    --tag=radstudio/paserver:alexandria --tag=radstudio/paserver:latest
+docker build . --build-arg password=securepass \
+	--tag=radstudio/paserver:latest \
+	--tag=radstudio/paserver:alexandria \
+	--tag=radstudio/paserver:11.3 \
