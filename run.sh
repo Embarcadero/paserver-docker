fc80b74a53ffe4e731264b925cdbfcba74c969d9
Serhii Kheilyk <1171495+lynxnake@users.noreply.github.com>
2024-01-24 16:50:04 +0200

    Restore --tty (Allocate pseudo-TTY) switch

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 echo "PAServer Password: securepass"
 docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-    --interactive --env PA_SERVER_PASSWORD=securepass \
+    --interactive --tty --env PA_SERVER_PASSWORD=securepass \
     -p 64211:64211 -p 8082:8082 radstudio/paserver:latest
